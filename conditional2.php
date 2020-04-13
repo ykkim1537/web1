c7647934c6dd912d63fbe2de56ea7b7cc926cbcf
ykkim1537 <37196542+ykkim1537@users.noreply.github.com>
2020-04-13 21:04:55 +0900

    Add files via upload

---
+++ conditional2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>조건 비교</h1>
+    <?php
+      $a = 5;
+      $b = 3;
+      if($a > $b){
+        echo "a is bigger than b";
+      }
+    ?>
+  </body>
+</html>
